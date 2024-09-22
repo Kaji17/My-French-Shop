fbfbd9236355dd656b7d23461c1d53ce358825e8
katina <ouattarapevirgile@gmail.com>
2024-09-22 21:55:23 +0000

    refactor: :recycle: ajout des proprietes de logo dans les variable d'env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  logoMfs: "./assets/img/logo-mfs.png"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  logoMfs: "./assets/img/logo-mfs.png"
 };
 
 /*
