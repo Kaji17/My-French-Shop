e3ac5ede3d6553d0c39442a886ef198440efca00
katina <ouattarapevirgile@gmail.com>
2024-09-29 22:39:17 +0000

    Add page auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentComponent } from './presentation/shared/components/layout/content/content.component';
 import { acuueiRoutes } from './presentation/shared/routes/accueil.routes';
+import { securityRoutes } from './presentation/shared/routes/security.routes';
 
 
 const routes: Routes = [
@@ -11,6 +12,16 @@ const routes: Routes = [
     children: [
       ...acuueiRoutes
     ]
+  },
+  ...securityRoutes,
+  {
+    path: "",
+    redirectTo: "",
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    redirectTo: ""
   }
 ];
 
--- src/app/presentation/pages/security/login/login.component.ts
+++ src/app/presentation/pages/security/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
+import { NzButtonSize } from 'ng-zorro-antd/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  size: NzButtonSize = 'large';
+  logoMfs: string = environment.logoMfs
 
+  constructor(
+    private route: Router
+  ) {
+  }
+
+  registerWithEmail(): void {
+    this.route.navigate(['auth/register'])
+  }
+  loginWithEmail(): void {
+    this.route.navigate(['auth/login/email'])
+  }
+  ngOnInit(): void {
+  }
 }
